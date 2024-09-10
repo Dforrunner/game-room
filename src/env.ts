58bbff0fe4618ab01d8e6e81333ba15c1789ff49
Mo <vtme996@gmail.com>
2024-09-09 23:01:59 -0500

    ip fix

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,6 @@
-const ip = process.env.NEXT_PUBLIC_LOCALHOST_IP;
+const ip = process.env.NEXT_PUBLIC_LOCALHOST_IP || '127.0.0.1';
 export const PARTYKIT_HOST =
   process.env.NEXT_PUBLIC_PARTYKIT_HOST ?? `${ip}:1999`;
-export const PROTOCOL = PARTYKIT_HOST.startsWith(ip || "127.0.0.1")
-  ? "http"
-  : "https";
+export const PROTOCOL = PARTYKIT_HOST.startsWith(ip) ? 'http' : 'https';
 export const PARTYKIT_URL = `${PROTOCOL}://${PARTYKIT_HOST}`;
 export const SERVER_URL = `${PROTOCOL}://${ip}:3000`;
